4b00c8e406bc9d7d8dcc0f09dca2a05ce9fef317
ninja-ops-guy <222743524+ninja-ops-guy@users.noreply.github.com>
2026-09-06 15:46:04 -0400

    release: expose certification API as 3.1.0

--- ruthless_pipeline/__init__.py
+++ ruthless_pipeline/__init__.py
@@ -1,17 +1,35 @@
-__version__ = "3.0.0"
+__version__ = "3.1.0"
 
 from .nap import BlackBoxMode, BlackBoxNAPConfig, EnhancedBlackBoxNAP
 from .deformation import NeuralDeformationConfig, DeformationObservation, NeuralDeformationModule, synthetic_observation
 from .physics import DifferentiablePhysicsConfig, DifferentiablePhysicsPipeline
 from .benchmark import BenchmarkConfig, CallableEvaluator, ComparativeBenchmark
+from .scene import GarmentSceneBatch, GarmentTextureComposer
+from .evaluators import DetectionBatch, TorchvisionDetectionEvaluator, DifferentiableScoreEvaluator
+from .certification import (
+    ArtifactBundle,
+    ArtifactRef,
+    Certificate,
+    CertificateDecision,
+    CertificationProtocol,
+    EvidenceState,
+    ModelManifest,
+    ModelRegistry,
+    PassCriteria,
+    PatternManifest,
+    issue_certificate,
+    load_protocol,
+    verify_certificate_bundle,
+)
 
 __all__ = [
     "BlackBoxMode", "BlackBoxNAPConfig", "EnhancedBlackBoxNAP",
     "NeuralDeformationConfig", "DeformationObservation", "NeuralDeformationModule", "synthetic_observation",
     "DifferentiablePhysicsConfig", "DifferentiablePhysicsPipeline",
     "BenchmarkConfig", "CallableEvaluator", "ComparativeBenchmark",
-    "GarmentSceneBatch", "GarmentTextureComposer", "TorchvisionDetectionEvaluator", "DifferentiableScoreEvaluator",
+    "GarmentSceneBatch", "GarmentTextureComposer", "DetectionBatch",
+    "TorchvisionDetectionEvaluator", "DifferentiableScoreEvaluator",
+    "ArtifactBundle", "ArtifactRef", "Certificate", "CertificateDecision",
+    "CertificationProtocol", "EvidenceState", "ModelManifest", "ModelRegistry",
+    "PassCriteria", "PatternManifest", "issue_certificate", "load_protocol", "verify_certificate_bundle",
 ]
-
-from .scene import GarmentSceneBatch, GarmentTextureComposer
-from .evaluators import TorchvisionDetectionEvaluator, DifferentiableScoreEvaluator
