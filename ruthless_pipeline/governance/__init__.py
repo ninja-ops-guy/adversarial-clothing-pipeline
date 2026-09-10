94e4564cef3e8ad4bcfedd9f044c05783c39272b
ninja-ops-guy <222743524+ninja-ops-guy@users.noreply.github.com>
2026-09-10 16:02:21 -0400

    governance pass7: export adaptive and chaos lifecycle APIs

--- ruthless_pipeline/governance/__init__.py
+++ ruthless_pipeline/governance/__init__.py
@@ -5,145 +5,16 @@ artifacts. See docs/experimental_governance/IMPLEMENTATION_PASSES.md.
 """
 
 from .ids import GovernanceId, IdKind
-from .ledger import (
-    GovernanceEvent,
-    GovernanceEventType,
-    GovernanceLedger,
-    LedgerIntegrityError,
-    payload_sha256,
-)
-from .state import (
-    ExperimentState,
-    GovernanceStateMachine,
-    InvariantConflict,
-    StateTransitionError,
-)
+from .ledger import GovernanceEvent, GovernanceEventType, GovernanceLedger, LedgerIntegrityError, payload_sha256
+from .state import ExperimentState, GovernanceStateMachine, InvariantConflict, StateTransitionError
 from .validators import GovernanceValidationError, validate_manifest
-from .constraints import (
-    FROZEN_D2_ARTIFACT_IDS,
-    ConstraintImpact,
-    ConstraintRegistryError,
-    ConstraintSet,
-    ConstraintSetRegistry,
-    EstimandImpact,
-    FrozenArtifactMutationError,
-    MigrationDecisionRecord,
-    MigrationImpactAnalysis,
-    MigrationOutcome,
-    TolerancePolicy,
-    assess_migration,
-    assert_frozen_d2_artifacts_unchanged,
-    classify_migration,
-)
+from .constraints import (FROZEN_D2_ARTIFACT_IDS, ConstraintImpact, ConstraintRegistryError, ConstraintSet, ConstraintSetRegistry, EstimandImpact, FrozenArtifactMutationError, MigrationDecisionRecord, MigrationImpactAnalysis, MigrationOutcome, TolerancePolicy, assess_migration, assert_frozen_d2_artifacts_unchanged, classify_migration)
 from .migration import record_migration_decision
-from .seal import (
-    CohortSeal,
-    GovernanceCohortSeal,
-    SealError,
-    TimestampAdapter,
-    seal_cohort,
-    seal_governed_cohort,
-    verify_governed_seal,
-    verify_seal,
-)
-from .bridge_overlap import (
-    BridgeGovernanceError,
-    OverlapEstimate,
-    RegimeDecision,
-    SentinelRecord,
-    assert_calibration_isolation,
-    classify_regime,
-    require_blind_re_evaluation,
-    require_pooling_legal,
-)
-from .cic_adapter import (
-    CICError,
-    CICResult,
-    ConstraintRepresentation,
-    SolveStatus,
-    SolverReply,
-    StructuralMetrics,
-    check as cic_check,
-    require_cross_encoding_agreement,
-)
-from .sampling import (
-    BackendSelection,
-    BackendTelemetry,
-    DiagnosticBundle,
-    DiagnosticPolicy,
-    RepresentativenessState,
-    SamplerLane,
-    SamplingGovernanceError,
-    SamplingRequest,
-    SamplingResult,
-    diagnose,
-    require_confirmatory_eligible,
-    select_backend,
-)
+from .seal import CohortSeal, GovernanceCohortSeal, SealError, TimestampAdapter, seal_cohort, seal_governed_cohort, verify_governed_seal, verify_seal
+from .bridge_overlap import BridgeGovernanceError, OverlapEstimate, RegimeDecision, SentinelRecord, assert_calibration_isolation, classify_regime, require_blind_re_evaluation, require_pooling_legal
+from .cic_adapter import CICError, CICResult, ConstraintRepresentation, SolveStatus, SolverReply, StructuralMetrics, check as cic_check, require_cross_encoding_agreement
+from .sampling import BackendSelection, BackendTelemetry, DiagnosticBundle, DiagnosticPolicy, RepresentativenessState, SamplerLane, SamplingGovernanceError, SamplingRequest, SamplingResult, diagnose, require_confirmatory_eligible, select_backend
+from .adaptive import AdaptiveGovernanceError, EvidenceSeal, FrozenPolicy, PolicyState, propose_next_wave_policy, require_policy_frozen_before_sampling
+from .chaos import ChaosArchive, ChaosCandidate, ChaosEvent, ChaosGovernanceError, ChaosStatus
 
-__all__ = [
-    "BackendSelection",
-    "BackendTelemetry",
-    "BridgeGovernanceError",
-    "CICError",
-    "CICResult",
-    "CohortSeal",
-    "ConstraintImpact",
-    "ConstraintRegistryError",
-    "ConstraintRepresentation",
-    "ConstraintSet",
-    "ConstraintSetRegistry",
-    "DiagnosticBundle",
-    "DiagnosticPolicy",
-    "EstimandImpact",
-    "ExperimentState",
-    "FROZEN_D2_ARTIFACT_IDS",
-    "FrozenArtifactMutationError",
-    "GovernanceCohortSeal",
-    "GovernanceEvent",
-    "GovernanceEventType",
-    "GovernanceId",
-    "GovernanceLedger",
-    "GovernanceStateMachine",
-    "GovernanceValidationError",
-    "IdKind",
-    "InvariantConflict",
-    "LedgerIntegrityError",
-    "MigrationDecisionRecord",
-    "MigrationImpactAnalysis",
-    "MigrationOutcome",
-    "OverlapEstimate",
-    "RegimeDecision",
-    "RepresentativenessState",
-    "SamplerLane",
-    "SamplingGovernanceError",
-    "SamplingRequest",
-    "SamplingResult",
-    "SealError",
-    "SentinelRecord",
-    "SolveStatus",
-    "SolverReply",
-    "StateTransitionError",
-    "StructuralMetrics",
-    "TimestampAdapter",
-    "TolerancePolicy",
-    "assess_migration",
-    "assert_calibration_isolation",
-    "assert_frozen_d2_artifacts_unchanged",
-    "cic_check",
-    "classify_migration",
-    "classify_regime",
-    "diagnose",
-    "payload_sha256",
-    "record_migration_decision",
-    "require_blind_re_evaluation",
-    "require_confirmatory_eligible",
-    "require_cross_encoding_agreement",
-    "require_pooling_legal",
-    "seal_cohort",
-    "seal_governed_cohort",
-    "select_backend",
-    "validate_manifest",
-    "verify_governed_seal",
-    "verify_seal",
-]
+__all__ = [name for name in globals() if not name.startswith("_")]
