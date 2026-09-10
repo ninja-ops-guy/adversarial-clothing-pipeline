037471aa2d6e2ac6242bc531f0ff1baf2ded5b00
ninja-ops-guy <222743524+ninja-ops-guy@users.noreply.github.com>
2026-09-10 14:39:36 -0400

    governance: expose adopted Pass 1 event and halt contracts

--- ruthless_pipeline/governance/__init__.py
+++ ruthless_pipeline/governance/__init__.py
@@ -5,8 +5,19 @@ artifacts. See docs/experimental_governance/IMPLEMENTATION_PASSES.md.
 """
 
 from .ids import GovernanceId, IdKind
-from .ledger import GovernanceEvent, GovernanceLedger, LedgerIntegrityError
-from .state import ExperimentState, GovernanceStateMachine, StateTransitionError
+from .ledger import (
+    GovernanceEvent,
+    GovernanceEventType,
+    GovernanceLedger,
+    LedgerIntegrityError,
+    payload_sha256,
+)
+from .state import (
+    ExperimentState,
+    GovernanceStateMachine,
+    InvariantConflict,
+    StateTransitionError,
+)
 from .validators import GovernanceValidationError, validate_manifest
 from .constraints import ConstraintImpact, ConstraintSet, MigrationOutcome, classify_migration
 from .seal import CohortSeal, SealError, seal_cohort, verify_seal
@@ -46,17 +57,53 @@ from .sampling import (
 )
 
 __all__ = [
-    "BackendSelection", "BackendTelemetry", "BridgeGovernanceError", "CICError",
-    "CICResult", "CohortSeal", "ConstraintImpact", "ConstraintRepresentation",
-    "ConstraintSet", "DiagnosticBundle", "DiagnosticPolicy", "ExperimentState",
-    "GovernanceEvent", "GovernanceId", "GovernanceLedger", "GovernanceStateMachine",
-    "GovernanceValidationError", "IdKind", "LedgerIntegrityError", "MigrationOutcome",
-    "OverlapEstimate", "RegimeDecision", "RepresentativenessState", "SamplerLane",
-    "SamplingGovernanceError", "SamplingRequest", "SamplingResult", "SealError",
-    "SentinelRecord", "SolveStatus", "SolverReply", "StateTransitionError",
-    "StructuralMetrics", "assert_calibration_isolation", "cic_check",
-    "classify_migration", "classify_regime", "diagnose", "require_blind_re_evaluation",
-    "require_confirmatory_eligible", "require_cross_encoding_agreement",
-    "require_pooling_legal", "seal_cohort", "select_backend", "validate_manifest",
+    "BackendSelection",
+    "BackendTelemetry",
+    "BridgeGovernanceError",
+    "CICError",
+    "CICResult",
+    "CohortSeal",
+    "ConstraintImpact",
+    "ConstraintRepresentation",
+    "ConstraintSet",
+    "DiagnosticBundle",
+    "DiagnosticPolicy",
+    "ExperimentState",
+    "GovernanceEvent",
+    "GovernanceEventType",
+    "GovernanceId",
+    "GovernanceLedger",
+    "GovernanceStateMachine",
+    "GovernanceValidationError",
+    "IdKind",
+    "InvariantConflict",
+    "LedgerIntegrityError",
+    "MigrationOutcome",
+    "OverlapEstimate",
+    "RegimeDecision",
+    "RepresentativenessState",
+    "SamplerLane",
+    "SamplingGovernanceError",
+    "SamplingRequest",
+    "SamplingResult",
+    "SealError",
+    "SentinelRecord",
+    "SolveStatus",
+    "SolverReply",
+    "StateTransitionError",
+    "StructuralMetrics",
+    "assert_calibration_isolation",
+    "cic_check",
+    "classify_migration",
+    "classify_regime",
+    "diagnose",
+    "payload_sha256",
+    "require_blind_re_evaluation",
+    "require_confirmatory_eligible",
+    "require_cross_encoding_agreement",
+    "require_pooling_legal",
+    "seal_cohort",
+    "select_backend",
+    "validate_manifest",
     "verify_seal",
 ]
